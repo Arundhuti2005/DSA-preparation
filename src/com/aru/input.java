228a7fb6705c72ee486bbf3c8fe809dfec0d8933
Arundhuti Jana <arundhutijana62@gmail.com>
2026-03-19 09:21:52 +0530

    Add 2nd day files

---
+++ src/com/aru/input.java
@@ -0,0 +1,14 @@
+package com.aru;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+//        System.out.print("please enter some input: ");
+//        int rollno=input.nextInt();
+//        System.out.println("your roll number is " +rollno);
+        int a = 230_000_000;
+        System.out.println( "value of a = "+a);
+    }
+}
---
+++ src/com/aru/primitives.java
@@ -0,0 +1,7 @@
+package com.aru;
+
+public class primitives {
+    public static void main(String[] args) {
+
+    }
+}
